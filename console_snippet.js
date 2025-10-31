c2e60ae49e19ee2d50d8ab07fddb1c0f6cb321de
Chris Freeman <christopher.m.freman37.ctr@army.mil>
2025-10-31 10:02:19 -0400

    initial repo stuff how tos

---
+++ console_snippet.js
@@ -0,0 +1,15 @@
+// Paste in your console for 1484_11 API
+var api = window.API_1484_11;
+api.SetValue("cmi.progress_measure","1");
+api.SetValue("cmi.score.scaled","1");
+api.SetValue("cmi.success_status","passed");
+api.SetValue("cmi.completion_status","completed");
+api.Commit(""); api.Terminate("");
+
+//Paste in your console for 1.2 API
+var api = window.API;
+api.SetValue("cmi.progress_measure","1");
+api.SetValue("cmi.score.scaled","1");
+api.SetValue("cmi.success_status","passed");
+api.SetValue("cmi.completion_status","completed");
+api.Commit(""); api.Terminate("");
